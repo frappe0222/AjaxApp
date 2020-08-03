f6895e14fc168fde179c9406ba68608fa1930c7c
frappe <frappeccino0222@gmail.com>
2020-08-03 12:11:25 +0900

    既読機能の実装

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -3,7 +3,29 @@ function check() {
   postsA = Array.from(posts);
 
   postsA.forEach(function (post) { 
-    post.addEventListener("click", (e) => { });
+    post.addEventListener("click", (e) => {
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/${postId}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        const item = XHR.response.post;
+        if (item.checked === true) {
+          post.setAttribute("data-check", "true");
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");
+        }
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        } else {
+          return null;
+        }
+      }
+      XHR.onerror = () => {
+        alert("Request failed");
+      };
+    });
   });
 }
 
