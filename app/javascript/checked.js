e2fce725085a2590b2a6cf292e7d12fc3483f7af
frappe <frappeccino0222@gmail.com>
2020-08-03 11:30:18 +0900

    要素を1つずつに対して、『クリック』」した際に動作するイベント駆動

---
+++ app/javascript/checked.js
@@ -0,0 +1,10 @@
+function check() {
+  const posts = document.getElementsByClassName("post");
+  postsA = Array.from(posts);
+
+  postsA.forEach(function (post) { 
+    post.addEventListener("click", (e) => { });
+  });
+}
+
+window.addEventListener("load", check);
